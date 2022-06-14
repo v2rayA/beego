139048fdfe7e8e302f1e83ee042741bf2bc9ac56
mzz2017 <m@mzz.pub>
2022-06-14 20:51:24 +0800

    fix: CreateFileAgainstSystemTunneling does not work

--- logs/file_time_windows.go
+++ logs/file_time_windows.go
@@ -4,22 +4,30 @@
 package logs
 
 import (
+	"fmt"
 	"syscall"
 )
 
 func CreateFileAgainstSystemTunneling(filename string) error {
-	pathp, err := syscall.UTF16PtrFromString(filename)
+	path, err := syscall.FullPath(filename)
 	if err != nil {
 		return err
 	}
-	h, err := syscall.CreateFile(pathp,
-		syscall.FILE_WRITE_ATTRIBUTES, syscall.FILE_SHARE_WRITE, nil,
-		syscall.OPEN_EXISTING, syscall.FILE_FLAG_BACKUP_SEMANTICS, 0)
+	pPath, err := syscall.UTF16PtrFromString(path)
 	if err != nil {
-		return err
+		return fmt.Errorf("UTF16PtrFromString: %w", err)
+	}
+	h, err := syscall.CreateFile(pPath,
+		syscall.FILE_WRITE_ATTRIBUTES, syscall.FILE_SHARE_WRITE, nil,
+		syscall.CREATE_NEW, syscall.FILE_FLAG_BACKUP_SEMANTICS, 0)
+	if err != nil {
+		return fmt.Errorf("CreateFile: %w", err)
 	}
 	defer syscall.Close(h)
 	var c syscall.Filetime
 	syscall.GetSystemTimeAsFileTime(&c)
-	return syscall.SetFileTime(h, &c, nil, nil)
+	if err:= syscall.SetFileTime(h, &c, nil, nil); err != nil {
+		return fmt.Errorf("SetFileTime: %w", err)
+	}
+	return nil
 }
